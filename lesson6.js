0a1f96fabd6e33a8f14edfc9c2a507a32c97e11d
Yevgenyi Kotvytskyi <yegeniy.kotvytskyi@gmail.com>
2020-06-11 13:22:32 +0300

    Start additional 06

--- lesson6.js
+++ lesson6.js
@@ -5,12 +5,17 @@
 alert, confirm, console, prompt
 */
 
+const maxTry = 10;
+const gameGoalNumber = 10;
+
+
 let isNumber = function (n) {
     return !isNaN(n) && isFinite(n);
 };
 
 function game() {
-    let goalNumber = 10;
+    let goalNumber = gameGoalNumber;
+    let tryCount = maxTry;
 
     function userTry(firstMessahe = '') {
         let tryNumber = prompt(`${firstMessahe}Введите число (Отмена - выход)`);
@@ -20,13 +25,22 @@ function game() {
         } else if (!isNumber(tryNumber)) {
             alert('Введте число!');
             userTry();
-        } else if (tryNumber < goalNumber) {
-            userTry('Загаданное число меньше!\n');
+        } else if ( tryCount == 1) {
+            if (confirm('Попытки закончились, хотите сыграть еще?')) {
+                tryCount = maxTry;
+                userTry();
+            }
+            alert('Игра окончена. Все попытки потрачены!');
+        }
+        else if (tryNumber < goalNumber) {
+            userTry(`Загаданное число больше ${tryNumber}! Осталось ${--tryCount}\n`);
         } else if (tryNumber > goalNumber) {
-            userTry('Загаданное число больше!\n');
+            userTry(`Загаданное число меньше ${tryNumber}! Осталось ${--tryCount}\n`);
         } else {
-            alert('Поздравляю! Вы угадали!');
-            return;
+            if ( confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?') ) {
+                tryCount = maxTry;
+                userTry();
+            } 
         }
     }
 
